255095dd07a9300e65acc1c33ae55890352692d8
foolan <foolanbhosale5@gmail.com>
2020-06-08 19:18:25 -0400

    added search functinality

--- zoodoc/views.py
+++ zoodoc/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import filters
 from .serializers import DoctorSerializer, ReviewSerializer
 from .models import Doctor, Review
 # Create your views here.
@@ -8,6 +9,8 @@ from .models import Doctor, Review
 class DoctorList(generics.ListCreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['zip_code', 'specialization','city']
 
 
 class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
